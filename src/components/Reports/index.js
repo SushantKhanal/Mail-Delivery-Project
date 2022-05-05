aac7d77a1e8c2ee22518b1dbaf5083fd759b2c12
SushantKhanal <dreamerking114@gmail.com>
2022-05-05 11:38:20 -0500

    mock-pdf-download

--- src/components/Reports/index.js
+++ src/components/Reports/index.js
@@ -1,10 +1,31 @@
 import * as React from "react";
-import Typography from "@mui/material/Typography";
+import { jsPDF } from "jspdf";
+import { Button } from "@mui/material";
 
-export default function Reports(props) {
-  return (
-    <div>
-      <Typography paragraph>No Past Orders to Show.</Typography>
-    </div>
-  );
+class Reports extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  generatePDF = () => {
+    var doc = new jsPDF("p", "pt");
+
+    doc.text(20, 20, "This is the first title.");
+    doc.addFont("helvetica", "normal");
+    doc.text(20, 60, "This is the second title.");
+    doc.text(20, 100, "This is the thrid title.");
+
+    doc.save("reports.pdf");
+  };
+
+  render() {
+    return (
+      <div>
+        <Button onClick={this.generatePDF}>Download PDF</Button>
+      </div>
+    );
+  }
 }
+
+export default Reports;
